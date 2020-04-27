d333c80589a65d10f3874d6dd38e893158990746
Cody Evans <1123code@gmail.com>
2020-04-26 20:41:39 -0700

    Added some testing?

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -19,4 +19,86 @@ TEST(PracticeTest, is_simple_palindrome)
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
+	actual = obj.isPalindrome("a");
+	ASSERT_TRUE(actual);
+	actual = obj.isPalindrome(" ");
+	ASSERT_TRUE(actual);
+	actual = obj.isPalindrome("aaa");
+	ASSERT_TRUE(actual);
+	actual = obj.isPalindrome("aaaa");
+	ASSERT_TRUE(actual);
+	actual = obj.isPalindrome("aa aa");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, sortDescending)
+{
+    Practice obj;
+	int one(1);
+	int two(2);
+	int three(3);
+    bool actual = true;
+
+	obj.sortDescending(one, two, three);
+	if(one < two && two < three) {
+		actual = true;
+	else {
+		actual = false;
+	}
+	ASSERT_TRUE(actual);
+	one = 1;
+	two = 3;
+	three = 2;
+
+	obj.sortDescending(one, two, three);
+	if(one < two && two < three) {
+		actual = true;
+	else {
+		actual = false;
+	}
+	ASSERT_TRUE(actual);
+	one = 2;
+	two = 1;
+	three = 3;
+
+	obj.sortDescending(one, two, three);
+	if(one < two && two < three) {
+		actual = true;
+	else {
+		actual = false;
+	}
+	ASSERT_TRUE(actual);
+	one = 2;
+	two = 3;
+	three = 1;
+
+	obj.sortDescending(one, two, three);
+	if(one < two && two < three) {
+		actual = true;
+	else {
+		actual = false;
+	}
+	ASSERT_TRUE(actual);
+	one = 3;
+	two = 1;
+	three = 2;
+
+	obj.sortDescending(one, two, three);
+	if(one < two && two < three) {
+		actual = true;
+	else {
+		actual = false;
+	}
+	ASSERT_TRUE(actual);
+	one = 3;
+	two = 2;
+	three = 1;
+
+	obj.sortDescending(one, two, three);
+	if(one < two && two < three) {
+		actual = true;
+	else {
+		actual = false;
+	}
+	ASSERT_TRUE(actual);
 }
